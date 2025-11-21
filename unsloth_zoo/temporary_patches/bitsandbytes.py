74aea1bcadfd2f6938f7c6474d9dab380ee1edf9
Aaron Casey <aaron.casey@availity.com>
2025-11-21 11:51:06 -0500

    Revert graph_break, restore previous state

--- unsloth_zoo/temporary_patches/bitsandbytes.py
+++ unsloth_zoo/temporary_patches/bitsandbytes.py
@@ -70,8 +70,7 @@ def patch_bitsandbytes_linear4bit_forward():
         # weight = self.weight.t() if self.weight.dim() == 2 else self.weight
 
         # Cannot do .t() on Params4bit, instead do it on torch.Tensor
-        torch._dynamo.graph_break()
-        weight = self.weight.transpose(0, 1)
+        weight = self.weight.as_subclass(torch.Tensor).t()
 
         return bitsandbytes.matmul_4bit(x, weight, bias=bias, quant_state=self.weight.quant_state).to(inp_dtype)
 
